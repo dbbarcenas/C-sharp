4386d1f603d2c8678bd2c2394719b0ef96bde8c6
durwin <dbbarcenas@gmail.com>
2013-10-01 20:52:30 -0400

    added recent project for advance application development

---
+++ Assignment1_DurwinB/WindowsFormsApplication1/Sales Bonus.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class SalesBonusForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SalesBonusForm));
+            this.LangGbox = new System.Windows.Forms.GroupBox();
+            this.FrenchRadioBttn = new System.Windows.Forms.RadioButton();
+            this.EnglishRadioButton = new System.Windows.Forms.RadioButton();
+            this.EmpNameTxtBox = new System.Windows.Forms.TextBox();
+            this.EmpIdTxtBox = new System.Windows.Forms.TextBox();
+            this.HrsWorkedTxtBox = new System.Windows.Forms.TextBox();
+            this.TotalSalesTxtBox = new System.Windows.Forms.TextBox();
+            this.SalesBonusTxtBox = new System.Windows.Forms.TextBox();
+            this.EmpNameLbl = new System.Windows.Forms.Label();
+            this.EmpIdLbl = new System.Windows.Forms.Label();
+            this.HrsWorkedLbl = new System.Windows.Forms.Label();
+            this.TotalSalesLbl = new System.Windows.Forms.Label();
+            this.SalesBonusLbl = new System.Windows.Forms.Label();
+            this.PrintBttn = new System.Windows.Forms.Button();
+            this.CalcBttn = new System.Windows.Forms.Button();
+            this.NextBttn = new System.Windows.Forms.Button();
+            this.LogoBox = new System.Windows.Forms.PictureBox();
+            this.printForm1 = new Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(this.components);
+            this.LangGbox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LangGbox
+            // 
+            this.LangGbox.Controls.Add(this.FrenchRadioBttn);
+            this.LangGbox.Controls.Add(this.EnglishRadioButton);
+            this.LangGbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LangGbox.Location = new System.Drawing.Point(177, 12);
+            this.LangGbox.Name = "LangGbox";
+            this.LangGbox.Size = new System.Drawing.Size(183, 112);
+            this.LangGbox.TabIndex = 0;
+            this.LangGbox.TabStop = false;
+            this.LangGbox.Text = "Language";
+            // 
+            // FrenchRadioBttn
+            // 
+            this.FrenchRadioBttn.AutoSize = true;
+            this.FrenchRadioBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FrenchRadioBttn.Location = new System.Drawing.Point(39, 59);
+            this.FrenchRadioBttn.Name = "FrenchRadioBttn";
+            this.FrenchRadioBttn.Size = new System.Drawing.Size(72, 19);
+            this.FrenchRadioBttn.TabIndex = 1;
+            this.FrenchRadioBttn.Text = "Français";
+            this.FrenchRadioBttn.UseVisualStyleBackColor = true;
+            this.FrenchRadioBttn.CheckedChanged += new System.EventHandler(this.FrenchRadioBttn_CheckedChanged);
+            // 
+            // EnglishRadioButton
+            // 
+            this.EnglishRadioButton.AutoSize = true;
+            this.EnglishRadioButton.Checked = true;
+            this.EnglishRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EnglishRadioButton.Location = new System.Drawing.Point(39, 36);
+            this.EnglishRadioButton.Name = "EnglishRadioButton";
+            this.EnglishRadioButton.Size = new System.Drawing.Size(66, 19);
+            this.EnglishRadioButton.TabIndex = 0;
+            this.EnglishRadioButton.TabStop = true;
+            this.EnglishRadioButton.Text = "English";
+            this.EnglishRadioButton.UseVisualStyleBackColor = true;
+            this.EnglishRadioButton.CheckedChanged += new System.EventHandler(this.EnglishRadioButton_CheckedChanged);
+            // 
+            // EmpNameTxtBox
+            // 
+            this.EmpNameTxtBox.Location = new System.Drawing.Point(188, 171);
+            this.EmpNameTxtBox.Name = "EmpNameTxtBox";
+            this.EmpNameTxtBox.Size = new System.Drawing.Size(167, 20);
+            this.EmpNameTxtBox.TabIndex = 1;
+            // 
+            // EmpIdTxtBox
+            // 
+            this.EmpIdTxtBox.Location = new System.Drawing.Point(188, 198);
+            this.EmpIdTxtBox.Name = "EmpIdTxtBox";
+            this.EmpIdTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.EmpIdTxtBox.TabIndex = 2;
+            // 
+            // HrsWorkedTxtBox
+            // 
+            this.HrsWorkedTxtBox.Location = new System.Drawing.Point(188, 236);
+            this.HrsWorkedTxtBox.Name = "HrsWorkedTxtBox";
+            this.HrsWorkedTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.HrsWorkedTxtBox.TabIndex = 3;
+            this.HrsWorkedTxtBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.HrsWorkedTxtBox_TextChanged);
+            // 
+            // TotalSalesTxtBox
+            // 
+            this.TotalSalesTxtBox.Location = new System.Drawing.Point(188, 262);
+            this.TotalSalesTxtBox.Name = "TotalSalesTxtBox";
+            this.TotalSalesTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.TotalSalesTxtBox.TabIndex = 4;
+            this.TotalSalesTxtBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TotalSalesTxtBox_TextChanged);
+            // 
+            // SalesBonusTxtBox
+            // 
+            this.SalesBonusTxtBox.Location = new System.Drawing.Point(188, 288);
+            this.SalesBonusTxtBox.Name = "SalesBonusTxtBox";
+            this.SalesBonusTxtBox.ReadOnly = true;
+            this.SalesBonusTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.SalesBonusTxtBox.TabIndex = 5;
+            // 
+            // EmpNameLbl
+            // 
+            this.EmpNameLbl.AutoSize = true;
+            this.EmpNameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmpNameLbl.Location = new System.Drawing.Point(32, 171);
+            this.EmpNameLbl.Name = "EmpNameLbl";
+            this.EmpNameLbl.Size = new System.Drawing.Size(102, 15);
+            this.EmpNameLbl.TabIndex = 6;
+            this.EmpNameLbl.Text = "Employee Name:";
+            // 
+            // EmpIdLbl
+            // 
+            this.EmpIdLbl.AutoSize = true;
+            this.EmpIdLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmpIdLbl.Location = new System.Drawing.Point(51, 198);
+            this.EmpIdLbl.Name = "EmpIdLbl";
+            this.EmpIdLbl.Size = new System.Drawing.Size(80, 15);
+            this.EmpIdLbl.TabIndex = 7;
+            this.EmpIdLbl.Text = "Employee ID:";
+            // 
+            // HrsWorkedLbl
+            // 
+            this.HrsWorkedLbl.AutoSize = true;
+            this.HrsWorkedLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HrsWorkedLbl.Location = new System.Drawing.Point(41, 236);
+            this.HrsWorkedLbl.Name = "HrsWorkedLbl";
+            this.HrsWorkedLbl.Size = new System.Drawing.Size(88, 15);
+            this.HrsWorkedLbl.TabIndex = 8;
+            this.HrsWorkedLbl.Text = "Hours Worked:";
+            // 
+            // TotalSalesLbl
+            // 
+            this.TotalSalesLbl.AutoSize = true;
+            this.TotalSalesLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalSalesLbl.Location = new System.Drawing.Point(58, 262);
+            this.TotalSalesLbl.Name = "TotalSalesLbl";
+            this.TotalSalesLbl.Size = new System.Drawing.Size(71, 15);
+            this.TotalSalesLbl.TabIndex = 9;
+            this.TotalSalesLbl.Text = "Total Sales:";
+            // 
+            // SalesBonusLbl
+            // 
+            this.SalesBonusLbl.AutoSize = true;
+            this.SalesBonusLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SalesBonusLbl.Location = new System.Drawing.Point(52, 288);
+            this.SalesBonusLbl.Name = "SalesBonusLbl";
+            this.SalesBonusLbl.Size = new System.Drawing.Size(79, 15);
+            this.SalesBonusLbl.TabIndex = 10;
+            this.SalesBonusLbl.Text = "Sales Bonus:";
+            // 
+            // PrintBttn
+            // 
+            this.PrintBttn.Location = new System.Drawing.Point(35, 325);
+            this.PrintBttn.Name = "PrintBttn";
+            this.PrintBttn.Size = new System.Drawing.Size(75, 23);
+            this.PrintBttn.TabIndex = 11;
+            this.PrintBttn.Text = "Print";
+            this.PrintBttn.UseVisualStyleBackColor = true;
+            this.PrintBttn.Click += new System.EventHandler(this.PrintBttn_Click);
+            // 
+            // CalcBttn
+            // 
+            this.CalcBttn.Location = new System.Drawing.Point(177, 325);
+            this.CalcBttn.Name = "CalcBttn";
+            this.CalcBttn.Size = new System.Drawing.Size(75, 23);
+            this.CalcBttn.TabIndex = 12;
+            this.CalcBttn.Text = "Calculate";
+            this.CalcBttn.UseVisualStyleBackColor = true;
+            this.CalcBttn.Click += new System.EventHandler(this.CalcBttn_Click);
+            // 
+            // NextBttn
+            // 
+            this.NextBttn.Location = new System.Drawing.Point(285, 325);
+            this.NextBttn.Name = "NextBttn";
+            this.NextBttn.Size = new System.Drawing.Size(75, 23);
+            this.NextBttn.TabIndex = 13;
+            this.NextBttn.Text = "Next";
+            this.NextBttn.UseVisualStyleBackColor = true;
+            this.NextBttn.Click += new System.EventHandler(this.NextBttn_Click);
+            // 
+            // LogoBox
+            // 
+            this.LogoBox.ErrorImage = null;
+            this.LogoBox.Image = ((System.Drawing.Image)(resources.GetObject("LogoBox.Image")));
+            this.LogoBox.Location = new System.Drawing.Point(12, 12);
+            this.LogoBox.Name = "LogoBox";
+            this.LogoBox.Size = new System.Drawing.Size(150, 140);
+            this.LogoBox.TabIndex = 14;
+            this.LogoBox.TabStop = false;
+            // 
+            // printForm1
+            // 
+            this.printForm1.DocumentName = "document";
+            this.printForm1.Form = this;
+            this.printForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter;
+            this.printForm1.PrinterSettings = ((System.Drawing.Printing.PrinterSettings)(resources.GetObject("printForm1.PrinterSettings")));
+            this.printForm1.PrintFileName = null;
+            // 
+            // SalesBonusForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 364);
+            this.Controls.Add(this.LogoBox);
+            this.Controls.Add(this.NextBttn);
+            this.Controls.Add(this.CalcBttn);
+            this.Controls.Add(this.PrintBttn);
+            this.Controls.Add(this.SalesBonusLbl);
+            this.Controls.Add(this.TotalSalesLbl);
+            this.Controls.Add(this.HrsWorkedLbl);
+            this.Controls.Add(this.EmpIdLbl);
+            this.Controls.Add(this.EmpNameLbl);
+            this.Controls.Add(this.SalesBonusTxtBox);
+            this.Controls.Add(this.TotalSalesTxtBox);
+            this.Controls.Add(this.HrsWorkedTxtBox);
+            this.Controls.Add(this.EmpIdTxtBox);
+            this.Controls.Add(this.EmpNameTxtBox);
+            this.Controls.Add(this.LangGbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SalesBonusForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sales Bonus";
+            this.LangGbox.ResumeLayout(false);
+            this.LangGbox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox LangGbox;
+        private System.Windows.Forms.RadioButton FrenchRadioBttn;
+        private System.Windows.Forms.RadioButton EnglishRadioButton;
+        private System.Windows.Forms.TextBox EmpNameTxtBox;
+        private System.Windows.Forms.TextBox EmpIdTxtBox;
+        private System.Windows.Forms.TextBox HrsWorkedTxtBox;
+        private System.Windows.Forms.TextBox TotalSalesTxtBox;
+        private System.Windows.Forms.TextBox SalesBonusTxtBox;
+        private System.Windows.Forms.Label EmpNameLbl;
+        private System.Windows.Forms.Label EmpIdLbl;
+        private System.Windows.Forms.Label HrsWorkedLbl;
+        private System.Windows.Forms.Label TotalSalesLbl;
+        private System.Windows.Forms.Label SalesBonusLbl;
+        private System.Windows.Forms.Button PrintBttn;
+        private System.Windows.Forms.Button CalcBttn;
+        private System.Windows.Forms.Button NextBttn;
+        private System.Windows.Forms.PictureBox LogoBox;
+        private Microsoft.VisualBasic.PowerPacks.Printing.PrintForm printForm1;
+    }
+}
+
---
+++ Assignment1_DurwinB/WindowsFormsApplication1/Sales Bonus.cs
@@ -0,0 +1,158 @@
+﻿/**
+ * App name: Sales Bonus 
+ * Programmer : Durwin Barcenas
+ * Date : Sept 22, 2013
+ * Description: This program will calculate the Sales Bonus 
+ *              based on the employee's Working hours and Total Sales.
+ * */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class SalesBonusForm : Form
+    {
+        public SalesBonusForm()
+        {
+            InitializeComponent();
+        }
+
+        // This method changes the labels from french to english when radio button is clicked 
+        private void EnglishRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+            RadioButton radioButton = (RadioButton)sender;
+                if (radioButton.Checked)
+                {
+                    
+                    LangGbox.Text = "Language";
+                    EmpNameLbl.Text = "Employee Name:";
+                    EmpIdLbl.Text = "Employee ID:";
+                    HrsWorkedLbl.Text = "Hours Worked:";
+                    TotalSalesLbl.Text = "Total Sales:";
+                    SalesBonusLbl.Text = "Sales Bonus:";
+                    PrintBttn.Text = "Print";
+                    NextBttn.Text = "Next";
+                    CalcBttn.Text = "Calculate";
+            }// end if
+        }// end of method EnglsihRadioButton
+
+        // This method changes the labels from english to french when button is clicked
+        private void FrenchRadioBttn_CheckedChanged(object sender, EventArgs e)
+        {
+           
+                RadioButton radioButton = (RadioButton)sender;
+                if (radioButton.Checked)
+                {
+                    LangGbox.Text = "Langue";
+                    EmpNameLbl.Text = "Nom de l'employé:";
+                    EmpIdLbl.Text = "Identifiant de l'employé:";
+                    HrsWorkedLbl.Text = "Heures travaillées:";
+                    TotalSalesLbl.Text = "Ventes totales:";
+                    SalesBonusLbl.Text = "Bonus des ventes:";
+                    PrintBttn.Text = "Imprimer";
+                    NextBttn.Text = "Prochaine";
+                    CalcBttn.Text = "Calculer";
+            }// end if 
+        }//end of method FrenchRadioBttn
+
+        // This method will calculate the Sales Bonus with these steps : 
+        //  Divide the Total Hours worked by 160, which will determine the percentage of hours worked.
+        //  Multiply the Total Montlhly Sales by 0.02, which is the Total Bonus Amount.
+        // Then Multiply the Percentage of Hours worked by Total Bonus Amount, which will result as the Sales Bonus. 
+        private void CalcBttn_Click(object sender, EventArgs e)
+        {
+            // Converting the text inputed in the textbox to double 
+            decimal inHoursWorked = Convert.ToDecimal(HrsWorkedTxtBox.Text);
+            decimal inTotalSales = Convert.ToDecimal(TotalSalesTxtBox.Text);
+
+            // formula for calculating the sales bonus
+            decimal percHours = inHoursWorked / 160;
+            decimal totalBonus = inTotalSales * 0.02m;
+            decimal salesBonus = percHours * totalBonus;
+
+            // validation for hours worked 
+            if (inHoursWorked <= 160)
+            {
+                //Displays the answer to the Sales Bonus Textbox with two decimal places
+                SalesBonusTxtBox.Text = "$" + Math.Round(salesBonus, 2);
+            }
+            else
+            {
+                MessageBox.Show(inHoursWorked + " hours worked is invalid, must be within 0 to 160 hours.");
+            } 
+
+            
+        }
+
+        // this method validates for numeric textbox only 
+        // Disabling the other keys except numbers
+        private void HrsWorkedTxtBox_TextChanged(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar)
+                 && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }// end if
+        }
+
+        // this method validates for numeric textbox only 
+        // Disabling the other keys except numbers
+        // and decimal point, that only alows 1 decimal point
+        private void TotalSalesTxtBox_TextChanged(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsControl(e.KeyChar)
+                 && !char.IsDigit(e.KeyChar)
+                 && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }// end if
+
+            // only allow one decimal point
+            if (e.KeyChar == '.'
+                && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }// end if
+        }// end of method TotalSalesTxtBox
+
+
+        // this method calls another method ClearTextBoxes
+        private void NextBttn_Click(object sender, EventArgs e)
+        {
+            ClearTextBoxes(this.Controls); 
+        }
+
+        // this method is for clearing all text in textboxes whenever 
+        //NextBttn_Click is being clicked 
+        private void ClearTextBoxes(Control.ControlCollection cc)
+        {
+            foreach (Control ctrl in cc)
+            {
+                TextBox textBox = ctrl as TextBox;
+                if (textBox != null)
+                {
+                    textBox.Clear();
+                }// end if
+                else {
+                    ClearTextBoxes(ctrl.Controls); 
+                }// end else
+            }// end foreach
+        }
+
+        private void PrintBttn_Click(object sender, EventArgs e)
+        {
+            // Print the form on the printer 
+            printForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPreview;
+            printForm1.Print(); 
+        }// end of method ClearTextBoxes
+    }
+}
